e48b4ff92c11c386113fe68cbb28ca670b845795
HenryFerreira <henryferreira133@gmail.com>
2022-04-03 15:08:06 -0300

    se desarrollo el modelo de recurso

--- src/main/java/co/com/sofka/biblioteca/models/Recurso.java
+++ src/main/java/co/com/sofka/biblioteca/models/Recurso.java
@@ -1,5 +1,75 @@
 package co.com.sofka.biblioteca.models;
 
-public class Recurso {
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDate;
+import java.util.Objects;
+import java.util.UUID;
+
+@Document(collection = "recursos")
+public class Recurso {
+    @Id
+    private String id = UUID.randomUUID().toString().substring(0, 10);
+    private String nombre;
+    private Boolean estado; //Prestado o Disponible (flase o true)
+    private LocalDate fechaPrestamo;
+    private String tipoRecurso;
+    private String categoriaRecurso;
+
+    public Recurso(String nombre, Boolean estado, LocalDate fechaPrestamo, String tipoRecurso, String categoriaRecurso) {
+        this.nombre = Objects.requireNonNull(nombre);
+        this.estado = estado;
+        this.fechaPrestamo = LocalDate.now();
+        this.tipoRecurso = Objects.requireNonNull(tipoRecurso);
+        this.categoriaRecurso = Objects.requireNonNull(categoriaRecurso);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+
+    public LocalDate getFechaPrestamo() {
+        return fechaPrestamo;
+    }
+
+    public void setFechaPrestamo(LocalDate fechaPrestamo) {
+        this.fechaPrestamo = fechaPrestamo;
+    }
+
+    public String getTipoRecurso() {
+        return tipoRecurso;
+    }
+
+    public void setTipoRecurso(String tipoRecurso) {
+        this.tipoRecurso = tipoRecurso;
+    }
+
+    public String getCategoriaRecurso() {
+        return categoriaRecurso;
+    }
+
+    public void setCategoriaRecurso(String categoriaRecurso) {
+        this.categoriaRecurso = categoriaRecurso;
+    }
 }
