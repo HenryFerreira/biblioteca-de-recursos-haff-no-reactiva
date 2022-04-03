a842d9e7423ae4189d36ef5fea5ad7d23cd77fb3
HenryFerreira <henryferreira133@gmail.com>
2022-04-03 15:06:02 -0300

    First commit. Se creo el modelo de recursos

---
+++ src/main/java/co/com/sofka/biblioteca/BibliotecaByHaffApplication.java
@@ -0,0 +1,13 @@
+package co.com.sofka.biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaByHaffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaByHaffApplication.class, args);
+	}
+
+}
